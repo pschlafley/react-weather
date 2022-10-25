8481bff2a878f460c8e83de3c612cd3733427783
Peyton Schlafley <pschlafley0@gmail.com>
2022-10-24 20:21:28 -0400

    created interfaces for the data types that I need to get the data from open weather map. Successfully pulled data from the api and got it to the screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+const api_key = import.meta.env.VITE_API_KEY;
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface IWeatherInfo {
+	description: string;
+	icon: string;
 }
 
-export default App
+interface IWeatherMainInfo {
+	temp: number;
+	feels_like: string;
+	humidity: number;
+	temp_max: number;
+	temp_min: number;
+}
+
+interface ICityWeatherData {
+	weather: Array<IWeatherInfo>;
+	name: string;
+	sys: object;
+	timezone: number;
+	visibility: number;
+	wind: object;
+	clouds: object;
+	main: IWeatherMainInfo;
+}
+
+interface IFormInput {
+	cityName: string;
+	stateName: string;
+}
+
+interface ICityInfo {
+	city: string;
+	state: string;
+}
+
+function App() {
+	const [formInput, setFormInput] = useState<IFormInput>({
+		cityName: '',
+		stateName: '',
+	});
+	const [getCityName, setCityName] = useState<ICityInfo>({
+		city: '',
+		state: '',
+	});
+	const [getCityWeatherData, setCityWeatherData] = useState<ICityWeatherData>();
+
+	const handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+		event.preventDefault();
+		let { name, value } = event.target as HTMLInputElement;
+		setFormInput({
+			...formInput,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+		event.preventDefault();
+
+		setCityName({
+			...getCityName,
+			city: formInput.cityName,
+			state: formInput.stateName,
+		});
+
+		try {
+			fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${formInput.cityName}&appid=${api_key}&units=imperial`
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					setCityWeatherData(data);
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="App">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="cityName">Enter City:</label>
+				<input name="cityName" type="text" onChange={handleChange} />
+				<input name="stateName" type="text" onChange={handleChange} />
+				<button type="submit">Submit</button>
+			</form>
+
+			<h2>Current City:</h2>
+			{getCityWeatherData !== undefined ? (
+				<>
+					<h3>
+						{getCityName.city} {getCityName.state}
+					</h3>
+
+					<div>
+						<h2>Today's Weather</h2>
+						<ul>
+							<li>Description: {getCityWeatherData.weather[0].description}</li>
+							<img
+								src={`http://openweathermap.org/img/wn/${getCityWeatherData.weather[0].icon}.png`}
+							></img>
+						</ul>
+					</div>
+				</>
+			) : (
+				<h3>Invalid Input</h3>
+			)}
+		</div>
+	);
+}
+
+export default App;
